8982f4141b5bd1a54add3c2cb7ccd9dd5877e2aa
sun510001 <sqf121@gmail.com>
2025-05-09 18:19:12 +0800

    update codes

---
+++ send_telegram_bot.py
@@ -0,0 +1,69 @@
+'''
+Author: sun510001 sqf121@gmail.com
+Date: 2025-05-09 18:05:03
+LastEditors: sun510001 sqf121@gmail.com
+LastEditTime: 2025-05-09 18:05:03
+FilePath: /stock_trading/tg_bot.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from telegram import Bot
+
+
+class TelegramNotifier:
+    def __init__(self, token, chat_id):
+        self.token = token
+        self.chat_id = chat_id
+
+    @staticmethod
+    def format_message(stock_id, current_info, indicators, suggestion):
+        lines = [f"📈 Market Snapshot -> {stock_id}"]
+        lines += [f"├ {k.capitalize():<6}: {v}" for k,
+                  v in current_info.items()]
+
+        lines.append("\n📊 Technical Indicators")
+        lines.append(f"├ RSI  : {indicators.get('RSI'):.2f}")
+        lines.append(f"├ MFI  : {indicators.get('MFI'):.2f}")
+
+        kdj = indicators.get("KDJ", {})
+        lines.append("├ KDJ")
+        lines.append(f"   ├ K: {kdj.get('K', 0):.2f}")
+        lines.append(f"   ├ D: {kdj.get('D', 0):.2f}")
+        lines.append(f"   └ J: {kdj.get('J', 0):.2f}")
+
+        lines.append(f"\n💡 Suggestion: {suggestion}")
+        return "\n".join(lines)
+
+    async def send_message(self, stock_id, current_info, indicators, suggestion):
+        bot = Bot(token=self.token)
+        text = self.format_message(stock_id, current_info, indicators, suggestion)
+        await bot.send_message(chat_id=self.chat_id, text=text)
+
+
+def main():
+    import asyncio
+    token = "xxxx"
+    chat_id = "xxxx"
+
+    current_info = {
+        'close': 29.265,
+        'high': 29.27,
+        'low': 29.22,
+        'volume': 1743.0
+    }
+    indicators = {
+        'RSI': 47.8806,
+        'MFI': 66.0124,
+        'KDJ': {
+            'K': 14.408,
+            'D': 13.629,
+            'J': 15.965
+        }
+    }
+    suggestion = "hold"
+
+    notifier = TelegramNotifier(token, chat_id)
+    asyncio.run(notifier.send_message(current_info, indicators, suggestion))
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
+'''
+Author: sun510001 sqf121@gmail.com
+Date: 2025-05-09 18:07:58
+LastEditors: sun510001 sqf121@gmail.com
+LastEditTime: 2025-05-09 18:07:58
+FilePath: /stock_trading/utils.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import pytz
 from datetime import datetime, time
 
 
-def get_time_difference_from_4am_ny():
-    ny_tz = pytz.timezone('America/New_York')
+def get_time_difference_from_ny(hour=9, minute=30):
+    ny_tz = pytz.timezone("America/New_York")
     now_ny = datetime.now(ny_tz)
-    four_am_ny = ny_tz.localize(datetime.combine(now_ny.date(), time(4, 0)))
+    four_am_ny = ny_tz.localize(datetime.combine(now_ny.date(), time(hour, minute)))
     time_difference = now_ny - four_am_ny
     return int(time_difference.total_seconds() / 60)
